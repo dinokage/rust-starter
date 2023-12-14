bd9e93e3708b7ae82a32d7dde0795ca95c333616
dinokage <yerradinesh01@gmail.com>
2023-12-14 11:45:38 +0530

    boiler-plate rusty

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io::stdin;
+
+fn main() {
+    let mut message:String = String::new();
+    println!("enter a message");
+
+    stdin().read_line(&mut message).unwrap();
+    println!("received message is: {}", message);
+
+    let my_snake_case_variable:String = String::from("All variables use snake case");
+    println!("{}", my_snake_case_variable);
+
+
+    let my_string:String   = String::from("Hello, World");
+    println!("{}", my_string)
+
+}
