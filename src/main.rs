5b3844eb97c316b75d5cbf9d6c2213f79b973222
dinokage <yerradinesh01@gmail.com>
2024-01-01 23:10:43 +0530

    learned datatypes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,123 @@
-use std::io::stdin;
+// use std::io::stdin;
 
+//structures have camelcase names
+#[derive(Debug)]
+struct User {
+    id: i64,
+    username: String,
+    skills: [String;3],
+    stack: Stack
+
+}
+
+#[derive(Debug)]
+enum Stack {
+    JS,
+    Rust,
+    Go,
+    Python
+}
 fn main() {
-    let mut message:String = String::new();
-    println!("enter a message");
+    // let mut message:String = String::new();
+    // println!("enter a message");
 
-    stdin().read_line(&mut message).unwrap();
-    println!("received message is: {}", message);
+    // stdin().read_line(&mut message).unwrap();
+    // println!("received message is: {}", message);
 
     let my_snake_case_variable:String = String::from("All variables use snake case");
     println!("{}", my_snake_case_variable);
 
 
-    let my_string:String   = String::from("Hello, World");
+    let my_string:String   = String::from("Snake Case variable here");
     println!("{}", my_string);
+    // integers
+    let my_integer: i32 =  69;
+    println!("{}", my_integer);
+    //parsing integers
+    let parsed_integer: i32 = "80085".parse().unwrap();
+    println!("Parsed integer is {}", parsed_integer);
+    println!("integer to string coversion : {}", parsed_integer.to_string());
+
+    // floating point
+    let my_float: f32 = 80.085;
+    println!("My float value : {}", my_float);
+    println!("Float floor : {}", my_float.floor());
+    println!("Float ceiling : {}", my_float.ceil());
+    println!("Float rounded : {}", my_float.round());
+
+    // working with floats and integers (type conversion)
+    let num1: i32 = 10;
+    let num2: f32 = 11.5;
+    // normal addition isn't possible as we cannot add integers to floating values in rust
+    println!("converting int to float, sum is : {}", num1 as f32+num2);
+    println!("converting float to int, sum is : {}", num1+num2 as i32);
+    // while converting float to int, we lose the precision offered by float. we need to be careful while converting float to int.
+    
+    //characters
+    let my_character: char = 'K';
+    println!("Is uppercase : {}", my_character.is_uppercase());
+    println!("Is lowercase : {}", my_character.is_lowercase());
+    println!("convert to lower case : {}" ,my_character.to_ascii_lowercase());
+    // observation : using .to_lowercase() method modifies the value stored in the variable. .to_ascii_lowercase() doesn't modify the stored value
+    println!("convert to string : {}", my_character.to_string());
+
+    //booleans
+    let my_boolean: bool = false;
+    assert_eq!(my_boolean, false);
+
+    //tuples
+    let my_tuple: (char,i32,f32,String) = ('K', 69, 80.085, "sulchik".to_string());
+    println!("char/int/float/string : {}/{}/{}/{}" , my_tuple.0, my_tuple.1, my_tuple.2, my_tuple.3);
+    
+    //destructuring tuples
+    let (letter, integer, float, string) = my_tuple;
+    println!("char/int/float/string : {}/{}/{}/{}" , letter, integer, float, string);
+    
+    //arrays
+    let my_array: [i32;5] = [8,0,0,8,5];
+
+    for num in my_array {
+        println!("number : {}", num);
+    }
+
+    let same_value_array: [i32;100] = [69;100];
+    println!("Array : {:?}", same_value_array);
+    println!("first element  :{}", same_value_array[0]);
+    println!("array length : {}" ,same_value_array.len());
+    println!("array size : {}", std::mem::size_of_val(&same_value_array));
+
+    //structs and enums
+
+    let dino: User = User {
+        id:80085,
+        username:String::from("dinokage"),
+        skills: ["React".to_string(), "Next".to_string(), "Node".to_string()],
+        stack: Stack::JS        
+    };
+
+    println!("username : {}", dino.username);
+    println!("skills : {:?}", dino.skills);
+
+    //combining structs with .. operator
+
+let tarun = User {
+    id: 80086,
+    username: String::from("scoobygamur"),
+    ..dino
+};
+
+println!("tarun : {:?}", tarun);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
